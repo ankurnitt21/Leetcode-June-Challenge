fc99d57fe508800e5c7832e90328c9d3c57dffcb
ranaankur39 <ranaankur39@gmail.com>
2020-06-10 13:15:11 +0530

    Day 10

---
+++ Search insert position.cpp
@@ -0,0 +1,23 @@
+// Time Complexity O(logn)
+// Space Complexity O(1)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int l=0,h=nums.size()-1;
+        
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            
+            if(nums[m]==target)
+                return m;
+            else if(nums[m]>target)
+                h=m-1;
+            else
+                l=m+1;
+        }
+        
+        return l;
+    }
+};
